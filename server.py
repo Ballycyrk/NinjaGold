c4d12e3740ebc211fa1f13b979663e42841eb842
Colin McKenna <colinjmckenna@gmail.com>
2015-11-06 12:38:19 -0800

    add random gold generator

--- server.py
+++ server.py
@@ -4,5 +4,21 @@ app = Flask(__name__)
 app.secret_key = "AineLucia"
 @app.route('/')
 def index():
+  try:
+    session['gold']
+  except:
+    session['gold'] = 0
+  print 'Back again'
+  return render_template('index.html')
+@app.route('/process_money', methods=['POST'])
+def gold():
+  if request.form['place'] == 'farm':
+    session['gold'] += random.randrange(10,21)
+  elif request.form['place'] == 'cave':
+    session['gold'] += random.randrange(5,11)
+  elif request.form['place'] == 'house':
+    session['gold'] += random.randrange(2,6)
+  else:
+    session['gold'] += random.randrange(-50,51)
   return render_template('index.html')
 app.run(debug=True)
