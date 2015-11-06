5e07878ddc850d12092288d04eb358827dce4ef4
Colin McKenna <colinjmckenna@gmail.com>
2015-11-06 15:20:46 -0800

    add activity log

--- server.py
+++ server.py
@@ -8,17 +8,34 @@ def index():
     session['gold']
   except:
     session['gold'] = 0
-  print 'Back again'
+  try:
+    session['activity']
+  except:
+    session['activity'] = []
   return render_template('index.html')
 @app.route('/process_money', methods=['POST'])
 def gold():
   if request.form['place'] == 'farm':
-    session['gold'] += random.randrange(10,21)
+    result = random.randrange(10,21)
   elif request.form['place'] == 'cave':
-    session['gold'] += random.randrange(5,11)
+    result = random.randrange(5,11)
   elif request.form['place'] == 'house':
-    session['gold'] += random.randrange(2,6)
+    result = random.randrange(2,6)
   else:
-    session['gold'] += random.randrange(-50,51)
-  return render_template('index.html')
+    result = random.randrange(-50,51)
+  session['gold'] += result
+
+  if result >= 0:
+    temp = 'win'
+    mess = 'You ' + str(abs(result)) + ' gold from the ' + request.form['place'] + '! '
+  else:
+    temp = 'lose'
+    mess = 'Entered a casino and lost ' + str(abs(result)) + ' gold... will you never learn? '
+  result = temp, mess
+  session['activity'].append(result)
+  print mess
+  print session['activity']
+  print result
+  print temp
+  return redirect ('/')
 app.run(debug=True)
